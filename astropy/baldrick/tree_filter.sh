4d2bc0c69e194bfd2fd81a44018c33caba3ced98
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-09-02 17:57:33 +0100

    Fixes to script for extracting baldrick code

--- astropy/baldrick/tree_filter.sh
+++ astropy/baldrick/tree_filter.sh
@@ -2,6 +2,8 @@
 
 mkdir -p baldrick/github
 mkdir -p baldrick/github/tests
+mkdir -p baldrick/blueprints
+mkdir -p baldrick/blueprints/tests
 
 if [ -d "changebot/github" ]; then
   mv changebot/github/* baldrick/github/;
@@ -25,6 +27,14 @@ fi
 
 if [ -f "changebot/tests/test_github_auth.py" ]; then
   mv changebot/tests/test_github_auth.py baldrick/github/tests/test_github_auth.py;
+fi
+
+if [ -f "changebot/blueprints/pull_request_checker.py" ]; then
+  mv changebot/blueprints/pull_request_checker.py baldrick/blueprints/pull_request_checker.py;
+fi
+
+if [ -f "changebot/blueprints/tests/test_pull_request_checker.py" ]; then
+  mv changebot/blueprints/tests/test_pull_request_checker.py baldrick/blueprints/tests/test_pull_request_checker.py;
 fi
 
 find . -maxdepth 1 -not -name "." -not -name ".." -not -name "setup.cfg" -not -name ".git" -not -name ".gitignore" -not -name "LICENSE" -not -name ".travis.yml" -not -name "requirements.txt" -not -name "baldrick" -exec rm -rf {} \;
